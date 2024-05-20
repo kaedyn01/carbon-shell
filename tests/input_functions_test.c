c37462b18799eeefbe81c4377271f74898a46e8c
Kaedyn <kaedynbolin1@gmail.com>
2024-05-19 22:16:58 -0500

    added unit tests

---
+++ tests/input_functions_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "input_functions.h"
+
+bool trim_whitespace_test() {
+    return true;
+}
+
+bool interpret_input() {
+    return true;
+}
+
+bool split_line() {
+    return true;
+}
+
+bool free_tokens() {
+    return true;
+}
+
+bool get_input_line() {
+    return true;
+}
+
+bool prompt() {
+    return true;
+}
+
+int main(void) {
+
+}
