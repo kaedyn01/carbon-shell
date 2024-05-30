68989e1d27a8ad35342b5295253c179ef288c44c
Kaedyn <kaedynbolin1@gmail.com>
2024-05-29 23:16:51 -0500

    Starting implementing built_in_functions_test.c

---
+++ tests/built_in_commands_test.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include "input_functions.h"
+
+bool ls_logic_test() {
+    printf(">>> Starting test for ls_logic() <<<\n");
+
+    return true;
+}
+
+bool cd_logic_test() {
+    printf(">>> Starting test for cd_logic() <<<\n");
+
+    return true;
+}
+
+bool mkdir_logic_test() {
+    printf(">>> Starting test for mkdir_logic() <<<\n");
+
+    return true;
+}
+
+bool touch_logic_test() {
+    printf(">>> Starting test for touch_logic() <<<\n");
+
+    return true;
+}
+
+bool echo() {
+    printf(">>> Starting test for echo_logic() <<<\n");
+
+    return true;
+}
+
+int main(void) {
+    return 0;
+}
