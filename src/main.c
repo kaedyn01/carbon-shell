be693516ef9c17a56a5ad079355db205cee0c77d
Kaedyn Bolin <kaedynbolin1@gmail.com>
2024-06-03 14:39:10 -0500

    Added ability to execute non-built in commands via system()

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include "built_in_commands.h"
 #include "input_functions.h"
 
@@ -16,14 +17,21 @@
  */
 void loop() {
     while (true) {
+		printf("-> ");
+
         char **args = get_args();
 
         if (args[0] == NULL) {
+
             free_args(args);
             continue;
+
         } else if (strcmp(args[0], "ls") == 0) {
+
             ls();
+
         } else if (strcmp(args[0], "cd") == 0) {
+
             int result = cd(args);
             if (result == 1) {
                 printf("Did not specify which directory to change to.\n");
@@ -31,16 +39,36 @@ void loop() {
             } else if (result == 2) {
                 printf("The directory entered doesn't exist, please try again.\n");
             }
+
         } else if (strcmp(args[0], "mkdir") == 0) {
+
 			mkdir_custom(args[1]);
+
         } else if (strcmp(args[0], "touch") == 0) {
+
             touch(args[1]);
+
         } else if (strcmp(args[0], "exit") == 0) {
+
+			free_args(args);
             return;
+			
         } else if (strcmp(args[0], "pwd") == 0) {
+
 			pwd();
+			
 		} else {
-            printf("csh: command not found: %s\n", args[0]);
+
+			char *command = strdup(args[0]);
+			for (int i = 1; args[i] != NULL; i++) {
+				printf("About to concat commands.\n");
+				strcat(command, " ");
+				strcat(command, args[i]);
+			}
+			system(command);
+
+			free(command);
+
         }
 
         free_args(args);
@@ -55,30 +83,6 @@ void loop() {
  * @return An integer indicating the shell ran successfully. 
  */
 int main(void) {
-
-	/**
-    char **args = get_args();
-
-    for (int i = 0; args[i] != NULL; i++) {
-        printf("\"%s\"\n", args[i]);
-    }
-    if (args == NULL) {
-        printf("args is NULL.\n");
-    }
-    if (args[0] == NULL) {
-        printf("First elem is NULL.\n");
-    }
-
-    printf("{ ");
-    for (int i = 0; args[i] != NULL; i++) {
-        for (int j = 0; args[i][j] != '\0'; j++) {
-            printf("\'%c\', ", args[i][j]);
-        }
-        printf("\n");
-    }
-    printf(" }\n");
-	*/
-
     loop();
 
     return 0;
